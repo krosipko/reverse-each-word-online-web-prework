3345e30951f082ba33594f81e3c45e2c24141673
Kyle Rosipko <krosipko@outlook.com>
2019-01-26 16:47:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentance)
-  r_sentance = []
-  r_sentance << sentance.split.collect {|x| x.reverse}
+  # r_sentance = []
+  r_sentance = sentance.split.collect {|x| x.reverse}
   end
   r_sentance.join(' ')
 end
